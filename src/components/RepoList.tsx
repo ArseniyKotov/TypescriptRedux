257c7f952a14f94f79e499d0e082e70a31a4be09
Arseniy Kotov <akotov@adobe.com>
2022-08-23 08:23:57 -0700

    updates

--- src/components/RepoList.tsx
+++ src/components/RepoList.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
-import { useActions } from "../hooks/useActions";
+import { useTypedSelector, useActions } from "../hooks";
 import { RootState } from "../state";
 
 export const RepoList = () => {
-  const state = useSelector((state: RootState) => state.repositories);
+  const { data, error, loading } = useTypedSelector(
+    (state: RootState) => state.repositories
+  );
   const [term, setTerm] = useState("");
   const { searchRepos } = useActions();
   const handleUpdateTerm = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -22,6 +23,9 @@ export const RepoList = () => {
         <input value={term} onChange={handleUpdateTerm} />
         <button>Search</button>
       </form>
+      {error && <h3>{error}</h3>}
+      {loading && <h3>Loading...</h3>}
+      {!error && !loading && data.map((piece) => <div>{piece}</div>)}
     </div>
   );
 };
